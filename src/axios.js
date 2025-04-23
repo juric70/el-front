012a2b8b4cac2d99203566e2c494ff8dcd8f4212
juric70 <andjela.juric12@gmail.com>
2025-04-23 03:38:55 +0200

    back conf added

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.baseURL = 'http://3.76.105.28/';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
